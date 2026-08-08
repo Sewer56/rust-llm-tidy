a6d4ea4fb03c9b72e2d03a97d553d3e672dcddf6
Sewer56 <admin@sewer56.dev>
2026-08-08 19:33:47 +0100

    Added: reorder change-record producer (ReorderMove + compute_moves) for dry-run reporting

--- src/rust-llm-tidy-reorder/src/lib.rs
+++ src/rust-llm-tidy-reorder/src/lib.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!(concat!("../", env!("CARGO_PKG_README")))]
 
-pub use reorder::Permutation;
+pub use reorder::{Permutation, ReorderMove, compute_moves};
 
 pub mod graph;
 pub mod reorder;
